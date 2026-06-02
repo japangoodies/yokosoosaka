d092dac621a95261ce61f28310efee0f6c88cd6b
noobita019 <noobita019@users.noreply.github.com>
2026-06-02 22:18:57 +0800

    bump service worker cache to v4 for new app.js

--- yokoso-website/sw.js
+++ yokoso-website/sw.js
@@ -1,5 +1,5 @@
-var CACHE = 'japangoodies-v3';
-var DATA_CACHE = 'japangoodies-data-v3';
+var CACHE = 'japangoodies-v4';
+var DATA_CACHE = 'japangoodies-data-v4';
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
