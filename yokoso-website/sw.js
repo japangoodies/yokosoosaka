052660bd6fda6d00cd2b8de6d41d8efbe3d640c3
noobita019 <noobita019@users.noreply.github.com>
2026-07-11 14:52:20 +0800

    Bump PWA cache version to force SW update with latest app.js fix

--- yokoso-website/sw.js
+++ yokoso-website/sw.js
@@ -1,5 +1,5 @@
-var CACHE = 'japangoodies-v9';
-var DATA_CACHE = 'japangoodies-data-v9';
+var CACHE = 'japangoodies-v10';
+var DATA_CACHE = 'japangoodies-data-v10';
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
@@ -8,7 +8,7 @@ self.addEventListener('install', function(e) {
         '.',
         'index.html',
         'css/style.css?v=2',
-        'js/app.js?v=20260621',
+        'js/app.js?v=20260711',
         'manifest.json'
       ]);
     })
